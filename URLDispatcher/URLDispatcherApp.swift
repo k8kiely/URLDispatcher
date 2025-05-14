635e99c765ca7eda1d84535a92e60ab9b40b033c
Kate Kiely <kate@pyxisgrowth.com>
2025-05-13 19:07:51 -0500

    Initial Commit

---
+++ URLDispatcher/URLDispatcherApp.swift
@@ -0,0 +1,17 @@
+//
+//  URLDispatcherApp.swift
+//  URLDispatcher
+//
+//  Created by Kate Kiely on 5/13/25.
+//
+
+import SwiftUI
+
+@main
+struct URLDispatcherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ URLDispatcherTests/URLDispatcherTests.swift
@@ -0,0 +1,17 @@
+//
+//  URLDispatcherTests.swift
+//  URLDispatcherTests
+//
+//  Created by Kate Kiely on 5/13/25.
+//
+
+import Testing
+@testable import URLDispatcher
+
+struct URLDispatcherTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
